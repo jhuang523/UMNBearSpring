aa8a857aa8d79de9a049ee24a041a65916b285d7
Jennifer W Huang <huan1428@umn.edu>
2025-03-09 15:22:19 -0500

    refactoring code

--- src/utils/config.py
+++ src/utils/config.py
@@ -3,11 +3,12 @@ import numpy as np
 import geopandas as gpd
 import pandas as pd
 import yaml
-import flopy
+import flopy as mf
 from utils.utils import *
+from utils.creeks import * 
 
 class Config:
-    """reads config info from yaml file"""
+    """reads config info from yaml file. contains domain and aquifer properties"""
     def __init__(self, yaml_file, **config):
         config = load_yaml(yaml_file)
         self.__dict__.update(**config)
@@ -15,6 +16,10 @@ class Config:
         self.load_geojsons()
         self.load_csvs()
     def __repr__(self):
+        try:
+            self.domain.plot()
+        except: 
+            print("can't display domain")
         return str(vars(self).keys())
     def load_geojsons(self):
         """Load geojsons provided in yaml file and adds the data (GeoDataFrame) as attributes keyed by their key in the yaml. 
@@ -58,8 +63,10 @@ class Config:
         print(f'merged {polygons}')
 
     def set_domain(self, polygon_id : str): 
-        """Takes polygon and assigns it as domain"""
-        self.domain = getattr(self, polygon_id)
+        """Takes polygon and returns gdf """
+        polygon =  getattr(self, polygon_id)
+        self.domain = gpd.GeoDataFrame(geometry = [polygon], crs = self.crs)
+        self.total_bounds = self.domain.total_bounds
         print(f'set domain to {polygon_id}')
 
     def apply_DEM_to_domain(self):
@@ -77,7 +84,126 @@ class Config:
                 print (f'added {feature_type}')
         except(KeyError) as e:
             print(e)        
-        
+    def load_creeks(self):
+        if isinstance(self.creeks, Creek):
+            print('you already have loaded creeks')
+        elif isinstance(self.creeks, gpd.GeoDataFrame):
+            self.creeks = Creek(geodf = self.creeks)
+            print('loaded new Creek object from creeks data')
+        else:
+            print('error')
+    def plot_polygons(self, *polygon_names, ax = None, **params):
+        if ax == None:
+            fig, ax = plt.subplots()
+        polygons = [getattr(self, p) for p in polygon_names]
+        gpd.GeoDataFrame(geometry = polygons, crs = self.crs).plot(ax = ax, **params)
+    def extract_grid_params_from_domain(self): 
+        """creates structured grid parameters based """
+        self.Lx = self.total_bounds[2] - self.total_bounds[0]
+        self.Ly = self.total_bounds[3] - self.total_bounds[1]
+        self.ncol = int(self.Lx/self.delc)
+        self.nrow = int(self.Ly/self.delr)
+        self.nlay = len(self.botm)
+        print(f"""extracted grid params: \nLx = {self.Lx}\nLy = {self.Ly}\nnrow = {self.nrow}\nncol = {self.ncol}\nnlay = {self.nlay}""")
+    
+    def extract_top_config(self):
+        if self.top_config == 'DEM':
+            self.top = self.dem_grid
+            print ('set top to DEM')
+        elif self.top_config == 'constant':
+            self.top = self.top_elev
+            print (f'set top to {self.top_elev}')
+        #TODO: add other configurations for custom top array reading in data ETC 
+
+    def extract_bottom_config(self):
+        botm_elev = []
+        for layer in self.botm:
+            if layer['layer_type'] == 'constant_slope':
+                azimuth = layer['azimuth']
+                dip = np.radians(layer['dip'])
+                slope = np.tan(dip)
+                azimuth_rad = np.radians(azimuth)
+                dx = self.delc
+                dy = self.delr
+                dz_dx = slope * np.cos(azimuth_rad)  # Change in Z per unit X
+                dz_dy = slope * np.sin(azimuth_rad)  # Change in Z per unit Y
+                x, y = np.meshgrid(np.arange(self.ncol), np.arange(self.nrow))  # Column (X) and Row (Y) indices
+                # Compute bottom elevation, subtracting elevation change from max elevation
+                if 'max_elev' in layer:
+                    z0 = layer['max_elev']
+                    layer_array = z0 - ((self.ncol - x-1)* dx * dz_dx) + ((self.nrow - y-1) * dy * dz_dy)
+                elif 'min_elev' in layer:
+                    z0 = layer['min_elev']
+                    layer_array = z0 + (x * dx * dz_dx) + (y * dy * dz_dy) #TODO: adjust this 
+                else:
+                    print("no starting elevation given, cannot create array")
+                    exit
+                botm_elev.append(layer_array)
+        self.botm_elev = np.stack(botm_elev, axis = 0)
+        print(f'set bottom array')
+
+    def create_grid(self, type = 'structured'):
+        try:
+            if type == 'structured':
+                delr = np.ones(self.ncol, dtype = float) * self.delr
+                delc = np.ones(self.nrow, dtype = float) * self.delc
+                self.model_grid = mf.discretization.StructuredGrid(
+                    lenuni = self.lenuni,
+                    nlay=self.nlay,
+                    nrow=self.nrow,
+                    ncol=self.ncol,
+                    delr=delr,
+                    delc=delc,
+                    top=self.top,
+                    botm=self.botm_elev,
+                    xoff=self.total_bounds[0],
+                    yoff=self.total_bounds[1],
+                    crs= self.crs
+                )
+            #TODO: configure for unstructured and other grid types 
+        except KeyError as e: 
+            print (f"error, missing grid param: {e}")
+        except e:
+            print(e)
+    def plot_model_grid_layers(self, **params):
+        azim = params.get('azim', None)
+        cmap = params.get('cmap', 'viridis')
+        elev = params.get('elev', None)
+        fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+        # y, x = np.meshgrid(np.arange(self.nrow) * self.delr + self.total_bounds[1],
+        #            np.arange(self.ncol) * self.delc + self.total_bounds[0])
+        x, y = np.meshgrid(np.arange(self.ncol) * self.delc + self.total_bounds[0], np.arange(self.nrow)* (-1 * self.delr) + self.total_bounds[3])
+        z = np.copy(self.botm_elev)
+        if hasattr(self, 'idomain'):
+            x[self.idomain[0] == 0] = np.nan
+            y[self.idomain[0] == 0] = np.nan
+            z[self.idomain == 0] = np.nan
+        for i in range(self.nlay):
+            ax.plot_surface(x, y, z[i], cmap = cmap)
+        ax.plot_surface(x, y, self.dem_grid)
+        ax.view_init(azim = azim, elev = elev)
+    def extract_idomain(self):
+        xc, yc = self.model_grid.xcellcenters, self.model_grid.ycellcenters #Get the cell center coords
+        points = np.array([(xc[i, j], yc[i, j]) for i in range(self.nrow) for j in range(self.ncol)])
+        #Extract the raw polygon from the GDF object subDomain
+        domain = self.domain.geometry.values[0]
+        mask = shp.vectorized.contains(domain, points[:, 0], points[:, 1])
+        idomain_mask = mask.reshape((self.nrow, self.ncol))
+        idomain = np.zeros((self.nlay, self.nrow, self.ncol), dtype=int)
+        idomain[:, :, :] = idomain_mask.astype(int)
+        self.idomain = idomain
+    def extract_K_values(self):
+        Kh_array = []
+        Kv_array = []
+        for K in self.Kh:
+            Kh_array.append(np.ones((self.ncol, self.nrow)) * K)
+        self.Kh_vals = np.stack(Kh_array, axis = 0)
+        for K in self.Kv:
+            Kv_array.append(np.ones((self.ncol, self.nrow)) * K)
+        self.Kv_vals = np.stack(Kv_array, axis = 0)
+        #TODO deal with more variable K values
+    # def assign_conduit_cells(self):
+    #     return
     def write_sim(self): 
         pass 
 
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -17,30 +17,30 @@ def load_geojson(path : str, crs = 'EPSG:32615'):
 def extract_polygon(geodf):
     """Takes geodataframe (already projected), extracts and returns polygon data"""
     polygon_data = geodf[geodf.geometry.type == 'Polygon']
-    polygon_data.loc[:,'geometry'] = polygon_data['geometry'].apply(shp.make_valid)
-    if polygon_data.is_valid.iloc[0]:
-        return polygon_data.explode(index_parts = True)
+    polygon = polygon_data['geometry'].apply(shp.make_valid)
+    if polygon.is_valid.iloc[0]:
+        return polygon.geometry.iloc[0]
     else:
         print('polygon not valid, check data and try again')
         return None
 def combine_polygons(*polygons, crs = 'EPSG:32615'): 
     """ Takes 2 polygons and returns a merged polygon that covers the max extent of both"""
-    all_edges = []
-    for p in polygons:
-        all_edges.append(p['geometry'])
-    return gpd.GeoDataFrame(geometry = [shp.ops.unary_union(all_edges)], crs = crs)
+    return shp.ops.unary_union(polygons)
 
 
-def apply_DEM_to_polygon(dem_path : str, polygon, delr, delc):
+def apply_DEM_to_polygon(dem_path : str, polygon_data, delr, delc, crs = 'EPSG:32615'):
     """Given DEM_path, polygon obj. and delr and delc, creates array of elevation values for the polygon. returns dem_grid"""
-    bounds = polygon.total_bounds 
+    if isinstance(polygon_data, shp.geometry.Polygon): 
+        bounds = gpd.GeoDataFrame(geometry = [polygon_data]).total_bounds 
+    elif isinstance(polygon_data, gpd.GeoDataFrame):
+        bounds = polygon_data.total_bounds
     width = int((bounds[2] - bounds[0])/delr)
     height = int((bounds[3] - bounds[1])/delc)
     with rasterio.open(dem_path) as src:
         #define the window of useful data(in UTM coords) from the larger DEM shapefile
         window = rasterio.windows.from_bounds(bounds[0], bounds[1], bounds[2], bounds[3], transform=src.transform)
         extent = rasterio.windows.bounds(window, src.transform)#get window boundaries to confirm they are correct
-        
+        print(extent)
         #convert all of the data to a grid so it can be used with the MODFLOW model
         # transform = rasterio.transform.from_bounds(*extent, width, height) #transform the data to the grid #TODO: what is this used for? 
         
@@ -56,7 +56,20 @@ def apply_DEM_to_polygon(dem_path : str, polygon, delr, delc):
         maxval = 10000
         dem_grid = np.ma.masked_where(dem_grid > maxval, dem_grid)
         return dem_grid
+def get_point(coords : tuple) -> shp.geometry.Point:
+    return shp.geometry.Point(coords)
 
+def get_nearest_point(geom1, geom2) -> shp.geometry.Point:
+    """Return the nearest point on geom2 to geom1. If a tuple is passed for either arg, then converts to a point"""
+    if type(geom1) == tuple:
+        geom1 = get_point(geom1)
+    if type(geom2) == tuple:
+        geom2 = get_point(geom2)
+    elif isinstance(geom1, shp.geometry.Polygon):
+        geom1 = geom1.boundary
+    elif isinstance(geom2, shp.geometry.Polygon):
+        geom2 = geom2.boundary
+    return shp.ops.nearest_points(geom1, geom2)[1]
 def bres(row1, col1, row2, col2):
     """Generate cell indices along a straight line in a 2D grid using Bresenham's algorithm."""
     cells = []
