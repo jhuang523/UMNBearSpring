c1a0976a850fa7cad225a0dd801db42e6c6c96ce
Jennifer W Huang <huan1428@umn.edu>
2025-07-11 10:05:03 -0500

    fixing verbosity

--- src/utils/creeks.py
+++ src/utils/creeks.py
@@ -24,25 +24,25 @@ class Creek:
     def add_new_segment(self, segment: shp.geometry.LineString):
         new_gpd = gpd.GeoDataFrame({'FID' :[max(self.data['FID']) + 1]}, geometry = [segment], crs = self.crs)
         self.data = pd.concat([self.data, new_gpd], axis = 0, ignore_index = True)
-    def remove_segment(self, *FID):
+    def remove_segment(self, verbose = False, *FID):
         self.data = self.data[self.data['FID'] not in FID]
-        print(f'removed segments {FID}')
-    def extend_creek(self, start, end): 
+        print_verbose(f'removed segments {FID}', verbose)
+    def extend_creek(self, start, end, verbose = False): 
         """take in start and end coordinates (x, y) and draw a straight line from start to end. add linestring to self.data"""
         if type(start) == tuple:
             start = get_point(start)
-            print('converted start tuple to Point')
+            print_verbose('converted start tuple to Point', verbose)
         if type(end) == tuple:
             end = get_point(end)
-            print('converted end tuple to Point')
+            print_verbose('converted end tuple to Point', verbose)
         new_segment = shp.geometry.LineString([start, end])
         self.add_new_segment(new_segment)
-        print(f'added segment from {start} to {end}')
-        print(self)
-    def clip_creek(self, geom, tolerance : float):
+        print_verbose(f'added segment from {start} to {end}', verbose)
+        print_verbose(self, verbose)
+    def clip_creek(self, geom, tolerance : float, verbose = False):
         """Removes segments outside of given geom. Tolerance is given in absolute value"""
         self.data = gpd.clip(self.data, geom.buffer(tolerance))
-        print("clipped creeks")
+        print_verbose("clipped creeks", verbose)
     def return_coordinates(self, FID):
         return self.return_segment(FID).geometry.coords
     def map_creek_to_grid(self, nrow, ncol, delr, delc, xmin, ymax):
