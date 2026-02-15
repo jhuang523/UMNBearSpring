7ecb81d720ce9a04e59f2ab784afc369f5180ca6
AD\huan1428 <huan1428@umn.edu>
2026-02-15 16:51:42 -0600

    fixing .sh script

--- openkarst/steady_state/conduit_geometry_test/ok_conduit_network_test.sh
+++ openkarst/steady_state/conduit_geometry_test/ok_conduit_network_test.sh
@@ -1,5 +1,5 @@
 #SBATCH --job-name=ok_conduit_network_test
-#SBATCH --time=
+#SBATCH --time=144:00:00
 #SBATCH --ntasks-per-node=128
 #SBATCH --mem=500gb
 #SBATCH --tmp=200g
@@ -13,4 +13,4 @@
 module load impi/2021/5.1
 module load conda
 source activate openkarst_2.0
-mpirun -np $SLURM_NTASKS python #put in task here 
+mpirun -np $SLURM_NTASKS python  ../../../src/scripts/openkarst/mpi_openkarst.py --sim_list_file sim_list.txt --verbose #put in task here 
