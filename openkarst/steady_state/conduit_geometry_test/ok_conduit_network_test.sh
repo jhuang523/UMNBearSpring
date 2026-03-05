d0ea6ac9ec8e0ac493dc8b81d3d6b35b08daf8ac
AD\huan1428 <huan1428@umn.edu>
2026-03-05 16:45:43 -0600

    gaussian spinup cases

--- openkarst/steady_state/conduit_geometry_test/ok_conduit_network_test.sh
+++ openkarst/steady_state/conduit_geometry_test/ok_conduit_network_test.sh
@@ -1,18 +1,18 @@
 #!/bin/bash -l        
 #SBATCH --job-name=ok_conduit_network_test
-#SBATCH --time=144:00:00
+#SBATCH --time=288:00:00
 #SBATCH --ntasks=10
 #SBATCH --mem=500gb
 #SBATCH --tmp=200g
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=huan1428@umn.edu
 #SBATCH -p pkkang
-#SBATCH --nodes=2
+#SBATCH --nodes=1
 #SBATCH --output=output/logs/job_%j.out
 #SBATCH --error=output/logs/job_%j.err
 
 module load impi/2021/5.1
 module load conda
 source activate openkarst
-mpirun -np $SLURM_NTASKS python  ../../../src/scripts/openkarst/mpi_openkarst.py --sim_list_file sim_list.txt --log_path "simulation_$(date +%Y%m%d_%H%M%S).log"
+mpirun -np $SLURM_NTASKS python  ../../../src/scripts/openkarst/mpi_openkarst.py --sim_list_file sim_list.txt --log_path "output/simulation_$(date +%Y%m%d_%H%M%S).log"
 )#put in task here 
