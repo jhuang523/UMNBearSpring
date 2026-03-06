17737147b43f48489c57747b8a3f1c10f171fbbb
AD\huan1428 <huan1428@umn.edu>
2026-03-06 14:01:55 -0600

    small diameter test

--- src/utils/openkarst_network.py
+++ src/utils/openkarst_network.py
@@ -231,30 +231,11 @@ class OpenKarstNetwork:
         self.geometry['throat.diameters'] = [edge_diameters[tuple(sorted(edge))] for edge in self.geometry['throat.conns']]
         return self.geometry
 
-    # def plot_network_flow(self, Q, h,  **params): #Q and h are arrays that match the number of edges (Q) and number of nodes (h)
-    #     import matplotlib.pyplot as plt
-    #     import seaborn as sns
-    #     from matplotlib.collections import LineCollection
+    def calculate_network_volume(self):
+        V = (self.geometry['throat.lengths'] * (self.geometry['throat.diameters']/2)**2 * np.pi).sum()
+        self.update_network(volume = V)
+        return V
 
-    #     axes = params.get("axes", ("x", "y"))
-    #     segments = self.edges[[f'{axes[0]}_0', f'{axes[1]}_0', f'{axes[0]}_1', f'{axes[1]}_1']].values.reshape(-1, 2, 2)
-    #     norm = plt.Normalize(vmin=Q.min(), vmax=Q.max())
-
-    #     # Get palette
-    #     palette = params.get("palette", "coolwarm")
-    #     cmap = sns.color_palette(palette, as_cmap=True)
-
-    #     lc = LineCollection(segments, cmap=cmap, norm=norm)
-    #     lc.set_array(Q)
-
-    #     fig, ax = plt.subplots(figsize=(6, 6))
-    #     ax.add_collection(lc)
-    #     ax.autoscale()  # Needed for LineCollection to be visible
-    #     sns.scatterplot(self.nodes, x = axes[0], y = axes[1], hue = h, ax = ax, palette = 'viridis')
-    #     plt.colorbar(lc, ax=ax, label="Flowrate (m³/s)")
-    #     plt.grid(True)
-    #     plt.legend(title = "Head")
-    
     def plot_3D_network(self, **params):
         node_color = params.get('node_color', None)
         edge_color = params.get('edge_color', None)
@@ -388,3 +369,27 @@ class OpenKarstNetwork:
         if save_path is not None:
             ani.save(save_path, writer="pillow", fps=15)
         plt.show()
+
+    # def plot_network_flow(self, Q, h,  **params): #Q and h are arrays that match the number of edges (Q) and number of nodes (h)
+    #     import matplotlib.pyplot as plt
+    #     import seaborn as sns
+    #     from matplotlib.collections import LineCollection
+
+    #     axes = params.get("axes", ("x", "y"))
+    #     segments = self.edges[[f'{axes[0]}_0', f'{axes[1]}_0', f'{axes[0]}_1', f'{axes[1]}_1']].values.reshape(-1, 2, 2)
+    #     norm = plt.Normalize(vmin=Q.min(), vmax=Q.max())
+
+    #     # Get palette
+    #     palette = params.get("palette", "coolwarm")
+    #     cmap = sns.color_palette(palette, as_cmap=True)
+
+    #     lc = LineCollection(segments, cmap=cmap, norm=norm)
+    #     lc.set_array(Q)
+
+    #     fig, ax = plt.subplots(figsize=(6, 6))
+    #     ax.add_collection(lc)
+    #     ax.autoscale()  # Needed for LineCollection to be visible
+    #     sns.scatterplot(self.nodes, x = axes[0], y = axes[1], hue = h, ax = ax, palette = 'viridis')
+    #     plt.colorbar(lc, ax=ax, label="Flowrate (m³/s)")
+    #     plt.grid(True)
+    #     plt.legend(title = "Head")
